3d0cb13bc58509007e1656d2e747b534ea555fd1
M Farhan ur Rasool <145224436+farhanjutt23@users.noreply.github.com>
2024-07-05 21:27:02 +0500

    loop

---
+++ loops/for.py
@@ -0,0 +1,4 @@
+'''in this program u shoul to print the numbers from the 1 to 100'''
+for i in range(1,100):
+    print(i)
+    
---
+++ loops/while.py
@@ -0,0 +1,6 @@
+''' in the while loop u should to print the numbers from the 1 to 100'''
+i=0
+while i < 10:
+    print(i)
+    i+=1
+    

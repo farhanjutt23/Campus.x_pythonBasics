67e309c866f0b4ebaae992083000f4dc45cbb1bf
M Farhan ur Rasool <145224436+farhanjutt23@users.noreply.github.com>
2024-07-05 22:07:39 +0500

    THe funcation

---
+++ Funactions/Sum.py
@@ -0,0 +1,10 @@
+"""write a program to add the any two numbers in the funcation"""
+def sum(a,b):
+    print("The sum is the",a+b)
+sum(2,3)
+sum(2,5)
+sum(2,7)
+sum(2,9)
+sum(2,2)
+sum(2,5)
+
---
+++ Funactions/return.py
@@ -0,0 +1,8 @@
+def sum(a,b):
+    sum=a+b
+
+    print(sum)
+    return sum
+    
+
+sum(2,4)
